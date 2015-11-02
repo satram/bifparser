333e2ad031d5d8c1a7cdd745adc143ebf570730f
Satheesh Ramalingam <satheesh.ramalingam@technicolor.com>
2015-11-02 14:00:22 -0800

    more OOPs refactoring

--- bifplayer/static/app.js
+++ bifplayer/static/app.js
@@ -1,23 +1,28 @@
 
-function BifParser(selected_movie) {
+function BifParser(selectedMovie) {
+    // private variable
     var that = this;
+
+    //instance variables
     this.fileSize = 0;
     this.timeStamp = 0;
     this.frameStartOffset = 0;
     this.frameSize = 0;
     this.dynamicUrl = 'http://' + window.location.hostname + ':' +
-        window.location.port + '/api/images/' + selected_movie + '.bif'
+        window.location.port + '/api/images/' + selectedMovie + '.bif'
     console.log(this.dynamicUrl)
-
     this.version = 0;
     this.numFrames = 0;
     this.timeStampMultiplier = 0;
     this.bifIndexTableLength = 0;
 
-    this.getFileSize = function () {
-        console.log("get filesize for ", this.dynamicUrl);
+    //--------- private members ------------------
+
+    //gets the size of online resource from its metadata
+    function getFileSize() {
+        console.log("get filesize for ", that.dynamicUrl);
         var request = jQuery.ajax({
-            url: this.dynamicUrl,
+            url: that.dynamicUrl,
             type: 'HEAD',
             success: function () {
                 that.fileSize = request.getResponseHeader("Content-Length");
@@ -25,32 +30,35 @@ function BifParser(selected_movie) {
             }
         });
     };
-    this.getFileSize();
 
-
-    this.hexToBase64 = function (str) {
-        return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+    // scale the given unsigned number, according to byte pos
+    function scaleNum(num, pos) {
+        var scaled = (num << (8 * pos)) >>> 0;
+        return scaled;
     };
 
-    this.renderImage = function (header) {
-        var img = document.createElement('img');
-        img.src = 'data:image/jpeg;base64,' + btoa(that.ab2str(header));
-        document.body.appendChild(img);
+    //read unsigned int from binary array
+    function readUint32(bytes, pos) {
+        var result = scaleNum(bytes[pos], 0) + scaleNum(bytes[pos + 1], 1) +
+            scaleNum(bytes[pos + 2], 2) + scaleNum(bytes[pos + 3], 3);
+        return result;
     };
 
-    this.ab2str = function (buf) {
+    //convert array buffer to string
+    function ab2str(buf) {
         return String.fromCharCode.apply(null, buf);
     };
 
-    this.strToByteArray = function (str) {
-        var byteArray, i;
-        byteArray = new Uint8Array(str.length);
-        for (i = 0, strLen = str.length; i < strLen; i++) {
-            byteArray[i] = (str.charCodeAt(i) & 0xFF) >>> 0;
-        }
-        return byteArray;
+    //--------- privileged members ------------------
+
+    // display the given binary data in html
+    this.renderImage = function (header) {
+        var img = document.createElement('img');
+        img.src = 'data:image/jpeg;base64,' + btoa(ab2str(header));
+        document.body.appendChild(img);
     };
 
+    //ajax wrapper
     this.downloadBinaryData = function (byterange, callback) {
         console.log("downloading", byterange, "in", this.dynamicUrl);
         jQuery.ajax({
@@ -67,40 +75,17 @@ function BifParser(selected_movie) {
         });
     };
 
-    this.downloadString = function (byterange, callback) {
-        console.log("downloading", byterange, "in", this.dynamicUrl);
-        jQuery.ajax({
-            url: this.dynamicUrl,
-            type: 'GET',
-            dataType: 'text',
-            headers: {Range: byterange}
-        }).done(function (buffer) {
-            console.log("downloaded", buffer.length, "bytes");
-            callback(buffer);
-        });
-    };
-
-    this.scaleNum = function (num, pos) {
-        var scaled = (num << (8 * pos)) >>> 0;
-        return scaled;
-    };
-
-    this.readUint32 = function (bytes, pos) {
-        var result = this.scaleNum(bytes[pos], 0) + this.scaleNum(bytes[pos + 1], 1) +
-            this.scaleNum(bytes[pos + 2], 2) + this.scaleNum(bytes[pos + 3], 3);
-        return result;
-    };
-
+    //parse BIF frame header
     this.parseHeader = function (header) {
         var bifIndexByteRange, oneIndexLen = 8;
 
-        that.version = that.readUint32(header, 8);
+        that.version = readUint32(header, 8);
         console.log("version", that.version);
 
-        that.numFrames = that.readUint32(header, 12);
+        that.numFrames = readUint32(header, 12);
         console.log("numFrames", that.numFrames);
 
-        that.timeStampMultiplier = that.readUint32(header, 16);
+        that.timeStampMultiplier = readUint32(header, 16);
         console.log("timeStampMultiplier", that.timeStampMultiplier);
 
         that.bifIndexTableLength = that.numFrames * oneIndexLen;
@@ -110,6 +95,7 @@ function BifParser(selected_movie) {
         that.downloadBinaryData(bifIndexByteRange, that.parseIndexTable);
     };
 
+    //parse BIF Index table
     this.parseIndexTable = function (header) {
         var i = 0, j = 0;
 
@@ -117,13 +103,13 @@ function BifParser(selected_movie) {
         that.frameStartOffset = new Uint32Array(that.numFrames);
         that.frameSize = new Uint32Array(that.numFrames);
 
-        that.timeStamp[j] = that.readUint32(header, i) * that.timeStampMultiplier;
-        that.frameStartOffset[j] = that.readUint32(header, i + 4);
+        that.timeStamp[j] = readUint32(header, i) * that.timeStampMultiplier;
+        that.frameStartOffset[j] = readUint32(header, i + 4);
 
         i = i + 8; j++;
         for (; i < that.bifIndexTableLength; j++, i = i + 8) {
-            that.timeStamp[j] = that.readUint32(header, i) * that.timeStampMultiplier;
-            that.frameStartOffset[j] = that.readUint32(header, i + 4);
+            that.timeStamp[j] = readUint32(header, i) * that.timeStampMultiplier;
+            that.frameStartOffset[j] = readUint32(header, i + 4);
             that.frameSize[j - 1] = that.frameStartOffset[j] - that.frameStartOffset[j - 1];
             console.log(
                 "Frame", j - 1, "timeStamp", that.timeStamp[j - 1],
@@ -136,15 +122,19 @@ function BifParser(selected_movie) {
             "frameStartOffset", that.frameStartOffset[j - 1],
             "frameSize", that.frameSize[j - 1]);
     };
+
+    getFileSize();
+
 }
 
+//public member
 BifParser.prototype.initHeader = function () {
     var bifHeaderByteRange, bifHeaderLength = 64;
     bifHeaderByteRange = "bytes=0-" + (bifHeaderLength - 1);
     this.downloadBinaryData(bifHeaderByteRange, this.parseHeader);
 }
 
-
+//public member
 BifParser.prototype.playFile = function () {
     console.log('number of entries in frameStartOffset', this.frameStartOffset.length);
     console.log('number of entries in frameSize', this.frameSize.length);
@@ -159,12 +149,13 @@ BifParser.prototype.playFile = function () {
     });
 }
 
+// wrappers for calling from html
 var bifParserInstance;
 
 function initialize() {
-    selected_movie = $('#movie :selected').text();
-    console.log(selected_movie);
-    bifParserInstance = new BifParser(selected_movie);
+    selectedMovie = $('#movie :selected').text();
+    console.log(selectedMovie);
+    bifParserInstance = new BifParser(selectedMovie);
     bifParserInstance.initHeader();
 }
 
